5530d16dab775d9e59ac1ed33c8b5b8ae74ab002
Shaun Prince <suparious@solidrust.net>
2023-04-07 18:17:58 -0700

    formatting fix

--- src/main.rs
+++ src/main.rs
@@ -4,163 +4,165 @@ use anyhow::Result;
 use clap::{App, Arg, SubCommand};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::path::PathBuf;
 use std::fs;
 use std::io::{Read, Write};
+use std::path::PathBuf;
 use steampunk_theme::steampunkify;
 
 #[derive(Serialize, Deserialize)]
 struct DataModel {
-    name: String,
-    path: PathBuf,
-    // Additional metadata fields can be added here.
+  name: String,
+  path: PathBuf,
+  // Additional metadata fields can be added here.
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let matches = App::new("Library Manager")
-        .about("Manage large data models in a steampunk-themed CLI")
-        .author("Your Name <your.email@example.com>")
+  let matches = App::new("Library Manager")
+    .about("Manage large data models in a steampunk-themed CLI")
+    .author("Your Name <your.email@example.com>")
+    .arg(
+      Arg::with_name("config")
+        .short("c")
+        .long("config")
+        .value_name("FILE")
+        .help("Sets a custom config file")
+        .takes_value(true),
+    )
+    .subcommand(
+      SubCommand::with_name("show")
+        .about("Show details of data models")
         .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .value_name("FILE")
-                .help("Sets a custom config file")
-                .takes_value(true),
+          Arg::with_name("MODEL")
+            .help("The data model to display details for")
+            .required(true)
+            .index(1),
+        ),
+    )
+    .subcommand(
+      SubCommand::with_name("move")
+        .about("Move a data model to a new location")
+        .arg(
+          Arg::with_name("MODEL")
+            .help("The data model to move")
+            .required(true)
+            .index(1),
         )
-        .subcommand(
-            SubCommand::with_name("show")
-                .about("Show details of data models")
-                .arg(
-                    Arg::with_name("MODEL")
-                        .help("The data model to display details for")
-                        .required(true)
-                        .index(1),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("move")
-                .about("Move a data model to a new location")
-                .arg(
-                    Arg::with_name("MODEL")
-                        .help("The data model to move")
-                        .required(true)
-                        .index(1),
-                )
-                .arg(
-                    Arg::with_name("DEST")
-                        .help("The new location for the data model")
-                        .required(true)
-                        .index(2),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("delete")
-                .about("Delete a data model")
-                .arg(
-                    Arg::with_name("MODEL")
-                        .help("The data model to delete")
-                        .required(true)
-                        .index(1),
-                ),
-        )
-        .get_matches();
+        .arg(
+          Arg::with_name("DEST")
+            .help("The new location for the data model")
+            .required(true)
+            .index(2),
+        ),
+    )
+    .subcommand(
+      SubCommand::with_name("delete")
+        .about("Delete a data model")
+        .arg(
+          Arg::with_name("MODEL")
+            .help("The data model to delete")
+            .required(true)
+            .index(1),
+        ),
+    )
+    .get_matches();
 
-    let config_file = matches.value_of("config").unwrap_or("config.json");
-    let mut data_models: HashMap<String, DataModel> = load_data_models(config_file)?;
+  let config_file = matches.value_of("config").unwrap_or("config.json");
+  let mut data_models: HashMap<String, DataModel> = load_data_models(config_file)?;
 
-    match matches.subcommand() {
-        ("show", Some(matches)) => {
-            let model_name = matches.value_of("MODEL").unwrap();
-            show_model(&data_models, model_name)?;
-        }
-        ("move", Some(matches)) => {
-            let model_name = matches.value_of("MODEL").unwrap();
-            let dest = matches.value_of("DEST").unwrap();
-            move_model(&mut data_models, model_name, dest).await?;
-            save_data_models(config_file, &data_models)?;
-        }
-        ("delete", Some(matches)) => {
-            let model_name = matches.value_of("MODEL").unwrap();
-            delete_model(&mut data_models, model_name).await?;
-            save_data_models(config_file, &data_models)?;
-        }
-        _ => {
-            println!("Invalid command. Use --help for more information.");
-        }
-    }    
+  match matches.subcommand() {
+    ("show", Some(matches)) => {
+      let model_name = matches.value_of("MODEL").unwrap();
+      show_model(&data_models, model_name)?;
+    }
+    ("move", Some(matches)) => {
+      let model_name = matches.value_of("MODEL").unwrap();
+      let dest = matches.value_of("DEST").unwrap();
+      move_model(&mut data_models, model_name, dest).await?;
+      save_data_models(config_file, &data_models)?;
+    }
+    ("delete", Some(matches)) => {
+      let model_name = matches.value_of("MODEL").unwrap();
+      delete_model(&mut data_models, model_name).await?;
+      save_data_models(config_file, &data_models)?;
+    }
+    _ => {
+      println!("Invalid command. Use --help for more information.");
+    }
+  }
 
-    Ok(())
+  Ok(())
 }
 
-    
 fn show_model(models: &HashMap<String, DataModel>, model_name: &str) -> Result<()> {
-    if let Some(model) = models.get(model_name) {
-        println!(
-            "{}\nName: {}\nPath: {}",
-            steampunkify("Model Details:"),
-            model.name,
-            model.path.display()
-        );
-    } else {
-        println!("Model '{}' not found.", model_name);
-    }
+  if let Some(model) = models.get(model_name) {
+    println!(
+      "{}\nName: {}\nPath: {}",
+      steampunkify("Model Details:"),
+      model.name,
+      model.path.display()
+    );
+  } else {
+    println!("Model '{}' not found.", model_name);
+  }
 
-    Ok(())
+  Ok(())
 }
-    
-async fn move_model(models: &mut HashMap<String, DataModel>, model_name: &str, dest: &str) -> Result<()> {
-    if let Some(model) = models.get_mut(model_name) {
-        let src = &model.path;
-        let dest_path = PathBuf::from(dest);
 
-        // Perform the actual move operation (this is a simple example, you may want to handle errors more gracefully)
-        tokio::fs::rename(src, &dest_path).await?;
+async fn move_model(
+  models: &mut HashMap<String, DataModel>,
+  model_name: &str,
+  dest: &str,
+) -> Result<()> {
+  if let Some(model) = models.get_mut(model_name) {
+    let src = &model.path;
+    let dest_path = PathBuf::from(dest);
 
-        // Update the model's path in the HashMap
-        model.path = dest_path;
+    // Perform the actual move operation (this is a simple example, you may want to handle errors more gracefully)
+    tokio::fs::rename(src, &dest_path).await?;
 
-        println!("Moved '{}' to '{}'", model_name, dest);
-    } else {
-        println!("Model '{}' not found.", model_name);
-    }
+    // Update the model's path in the HashMap
+    model.path = dest_path;
 
-    Ok(())
+    println!("Moved '{}' to '{}'", model_name, dest);
+  } else {
+    println!("Model '{}' not found.", model_name);
+  }
+
+  Ok(())
 }
-    
+
 async fn delete_model(models: &mut HashMap<String, DataModel>, model_name: &str) -> Result<()> {
-    if let Some(model) = models.remove(model_name) {
-        // Perform the actual delete operation (this is a simple example, you may want to handle errors more gracefully)
-        tokio::fs::remove_dir_all(&model.path).await?;
+  if let Some(model) = models.remove(model_name) {
+    // Perform the actual delete operation (this is a simple example, you may want to handle errors more gracefully)
+    tokio::fs::remove_dir_all(&model.path).await?;
 
-        println!("Deleted '{}'", model_name);
-    } else {
-        println!("Model '{}' not found.", model_name);
-    }
+    println!("Deleted '{}'", model_name);
+  } else {
+    println!("Model '{}' not found.", model_name);
+  }
 
-    Ok(())
+  Ok(())
 }
-    
 
 fn load_data_models(config_file: &str) -> Result<HashMap<String, DataModel>> {
-    let mut file = fs::File::open(config_file).or_else(|_| fs::File::create(config_file))?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+  let mut file = fs::File::open(config_file).or_else(|_| fs::File::create(config_file))?;
+  let mut contents = String::new();
+  file.read_to_string(&mut contents)?;
 
-    let data_models: HashMap<String, DataModel> = if contents.is_empty() {
-        HashMap::new()
-    } else {
-        serde_json::from_str(&contents)?
-    };
+  let data_models: HashMap<String, DataModel> = if contents.is_empty() {
+    HashMap::new()
+  } else {
+    serde_json::from_str(&contents)?
+  };
 
-    Ok(data_models)
+  Ok(data_models)
 }
 
 fn save_data_models(config_file: &str, data_models: &HashMap<String, DataModel>) -> Result<()> {
-    let contents = serde_json::to_string(data_models)?;
-    let mut file = fs::File::create(config_file)?;
-    file.write_all(contents.as_bytes())?;
+  let contents = serde_json::to_string(data_models)?;
+  let mut file = fs::File::create(config_file)?;
+  file.write_all(contents.as_bytes())?;
 
-    Ok(())
+  Ok(())
 }
